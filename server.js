f33a4150c76b33873c313d9311f8e858dbe9c13f
Tahkk <152865115+BNoteris@users.noreply.github.com>
2023-12-09 11:48:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ let confirmationMessage = ''
 var mysql= require("mysql2")
 var connection = mysql.createConnection({
     host: '127.0.0.1',
+    port : '3306',
     user: 'root',
     password:'password',
     database:'taches'
