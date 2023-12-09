b1ca0867df2547b0bc03ca49bfd08593e937c7e9
Tahkk <152865115+BNoteris@users.noreply.github.com>
2023-12-09 11:45:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/effacerTaches/:index', (req, res) => {
 
 app.use(express.static("public"))
 
-app.listen(80, function(){
-    console.log("Server is running on port 80")
+app.listen(61817, function(){
+    console.log("Server is running on port 61817")
 })
 
